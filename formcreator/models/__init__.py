530a530082331ebb8f343de4d03d8fbb8e96b5f3
Edgar Z. Alvarenga <edgar@ymonad.com>
2010-12-17 15:03:41 -0200

    Creating models/ and view/

---
+++ formcreator/models/__init__.py
@@ -0,0 +1,27 @@
+import transaction
+
+from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+from zope.sqlalchemy import ZopeTransactionExtension
+
+DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
+Base = declarative_base()
+
+def populate():
+    session = DBSession()
+    session.flush()
+    transaction.commit()
+
+def initialize_sql(db_string, db_echo=False):
+    engine = create_engine(db_string, echo=db_echo)
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+    Base.metadata.create_all(engine)
+    try:
+        populate()
+    except IntegrityError:
