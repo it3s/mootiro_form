0eafe5a0e9058a37e824efa364bf4c49f6ac93b3
Nando Florestan <nandoflorestan@gmail.com>
2010-12-23 14:08:25 -0200

    Adding pyramid_beaker to setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# http://peak.telecommunity.com/DevCenter/setuptools#developer-s-guide
+
 import os
 import sys
-
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -9,12 +11,13 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'pyramid',
-    'SQLAlchemy',
+    'pyramid >= 1.0a7',
+    'SQLAlchemy >= 0.6.5',
     'transaction',
     'repoze.tm2',
     'zope.sqlalchemy',
     'WebError',
+    'pyramid_beaker >= 0.2',
     ]
 
 if sys.version_info[:3] < (2,5,0):
@@ -22,18 +25,21 @@ if sys.version_info[:3] < (2,5,0):
 
 setup(name='formcreator',
       version='0.0',
-      description='formcreator',
+      description='A web tool that lets you create forms, collect ' \
+          'information and generate reports',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
+        "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
         "Framework :: Pylons",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
         ],
       author='it3s',
       author_email='',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
+      url='http://mootiro.org/',
+      keywords='web forms wsgi pyramid',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
