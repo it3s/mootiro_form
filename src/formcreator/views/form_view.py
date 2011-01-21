804bb141f093d6399affbbc57dd24819321453b6
Nando Florestan <nandoflorestan@gmail.com>
2011-01-21 16:49:30 -0200

    Improvements to the form editor, but the view still contains pasted user view code.

--- src/formcreator/views/form_view.py
+++ src/formcreator/views/form_view.py
@@ -9,7 +9,7 @@ from pyramid.httpexceptions import HTTPFound
 from pyramid.response import Response
 from pyramid.security import remember, forget
 from pyramid_handlers import action
-from ..models import User, sas
+from ..models import User, Form, sas
 from . import BaseView
 
 
@@ -20,11 +20,11 @@ class FormView(BaseView):
     @action(name='new', renderer='form_edit.genshi', request_method='GET')
     def new_form(self):
         '''Displays a new form, ready for editing.'''
-        return dict(pagetitle=self.CREATE_TITLE)
+        return dict(pagetitle=self.CREATE_TITLE, form=Form())
 
-    @action(name='new', renderer='user_edit.genshi', request_method='POST')
+    @action(name='new', renderer='form_edit.genshi', request_method='POST')
     def create(self):
-        '''Creates a new User from POSTed data if it validates;
+        '''Creates a new Form from POSTed data if it validates;
         else redisplays the form with the error messages.
         '''
         controls = self.request.params.items()
@@ -39,28 +39,15 @@ class FormView(BaseView):
         sas.flush()
         return self.authenticate(u.id)
 
-    def authenticate(self, user_id):
-        '''Stores the user_id in a cookie, for subsequent requests.'''
-        headers = remember(self.request, user_id) # really say user_id here?
-        # May also set max_age above. (pyramid.authentication, line 272)
-
-        # Alternate implementation:
-        # headers = remember(self.request, Authenticated)
-        # May also set max_age above. (pyramid.authentication, line 272)
-
-        # Another way would be to implement session-based auth/auth.
-        # session['user_id'] = user_id
-        return HTTPFound(location='/', headers=headers)
-
-    @action(name='current', renderer='user_edit.genshi', request_method='GET')
-    def edit_user(self):
-        '''Displays the form to edit the current user profile.'''
+    @action(renderer='form_edit.genshi', request_method='GET')
+    def edit(self):
+        '''Displays the form editor, for an existing form.'''
         user = self.request.user
         return dict(pagetitle=self.EDIT_TITLE,
             user_form=user_form().render(self.model_to_dict(user,
                 ('nickname', 'real_name', 'email', 'password'))))
 
-    @action(name='current', renderer='user_edit.genshi', request_method='POST')
+    @action(name='current', renderer='form_edit.genshi', request_method='POST')
     def save_user(self):
         '''Saves the user profile from POSTed data if it validates;
         else redisplays the form with the error messages.
