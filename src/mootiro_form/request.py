f6354409f11bad86cbc399b72df8529b6a27a240
Edgar Z. Alvarenga <edgar@ymonad.com>
2011-02-08 11:48:56 -0200

    Removing absolute addressing in url references.

--- src/mootiro_form/request.py
+++ src/mootiro_form/request.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals # unicode by default
 from pyramid.decorator import reify
 from pyramid.request import Request
 from pyramid.security import authenticated_userid
+from pyramid.url import route_url
 from mootiro_form.models import User, sas
 from mootiro_web.page_deps import DepsRegistry, PageDeps
 
@@ -19,27 +20,26 @@ def init_deps(settings):
     # The first URL is for development (uncompressed js, can be debugged)
     # The second URL is for production (Google CDN, fastest)
     # The third URL is for production (static, for when Google is out).
-    deps.lib('jquery', ('/static/lib/jquery-1.5.js',
-        'https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js',
-        '/static/lib/jquery-1.5.min.js'))
+    deps.lib('jquery', ('http://' + settings['url_root'] + 'static/lib/jquery-1.5.js',\
+        'https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js'))
         # also possible: /deform/scripts/jquery-1.4.2.min.js
-    deps.lib('deform', '/deform/scripts/deform.js', depends='jquery')
-    deps.stylesheet('deform1', '/deform/css/form.css')
-    deps.stylesheet('deform2', '/deform/css/theme.css')
+    deps.lib('deform', 'http://' + settings['url_root'] + 'deform/scripts/deform.js', depends='jquery')
+    deps.stylesheet('deform1', 'http://' + settings['url_root'] + 'deform/css/form.css')
+    deps.stylesheet('deform2', 'http://' + settings['url_root'] + 'deform/css/theme.css')
     deps.package('deform', libs='deform', css='deform1|deform2',
                  onload='deform.load();')
-    deps.lib('jquery.ui', ('/static/lib/jquery-ui-1.8.9.min.js',
+    deps.lib('jquery.ui', ('http://' + settings['url_root'] + 'static/lib/jquery-ui-1.8.9.min.js',
         'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js',
-        '/static/lib/jquery-ui-1.8.9.min.js'), depends='jquery')
-    deps.stylesheet('jquery.ui', 'http://ajax.googleapis.com/ajax/libs/' \
-                    'jqueryui/1.8.8/themes/base/jquery-ui.css')
+        'http://' + settings['url_root'] + 'static/lib/jquery-ui-1.8.9.min.js'), depends='jquery')
+    deps.stylesheet('jquery.ui', 'http://ajax.googleapis.com/ajax/libs/',\
+                    'http://' + settings['url_root'] + 'jqueryui/1.8.8/themes/base/jquery-ui.css')
     deps.package('jquery.ui', libs='jquery.ui', css='jquery.ui')
-    deps.lib('infieldlabel', '/static/lib/jquery.infieldlabel.min.js',
+    deps.lib('infieldlabel', 'http://' + settings['url_root'] + 'static/lib/jquery.infieldlabel.min.js',
              depends='jquery')
-    deps.stylesheet('master_global', '/static/css/master_global.css')
-    deps.stylesheet('master_logged', '/static/css/master_logged.css')
-    deps.stylesheet('master_cover' , '/static/css/master_cover.css')
-    deps.stylesheet('form_edit'    , '/static/css/form_edit.css')
+    deps.stylesheet('master_global', 'http://' + settings['url_root'] + 'static/css/master_global.css')
+    deps.stylesheet('master_logged', 'http://' + settings['url_root'] + 'static/css/master_logged.css')
+    deps.stylesheet('master_cover' , 'http://' + settings['url_root'] + 'static/css/master_cover.css')
+    deps.stylesheet('form_edit'    , 'http://' + settings['url_root'] + 'static/css/form_edit.css')
 
 
 class MyRequest(Request):
