03bf463f8f56bd939f54dd4fd96be64859555a23
Nando Florestan <nandoflorestan@gmail.com>
2011-02-11 15:14:05 -0200

    Lets the user know the nickname is immutable.

--- src/mootiro_form/schemas/user.py
+++ src/mootiro_form/schemas/user.py
@@ -44,7 +44,8 @@ password  = c.SchemaNode(c.Str(), title=_('Password'),
 
 class CreateUserSchema(c.MappingSchema):
     nickname  = c.SchemaNode(c.Str(), title=_('Nickname'),
-        description=_("a short name for you, without spaces"), size=20,
+        description=_("a short name for you, without spaces. " \
+                      "This cannot be changed later!"), size=20,
         validator=c.All(c.Length(**LEN_NICKNAME), unique_nickname))
     real_name = real_name
     email     = email
