1f09462f20bb79d33512b66565caa3ee2a599a85
Nando Florestan <nandoflorestan@gmail.com>
2011-02-15 19:41:34 -0200

    Improve text.random_word() and delete string.py

--- src/mootiro_form/utils/string.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def random_word(wordLen):
-    '''Returns a random string with worLen length'''
-    word = ''
-    for i in range(wordLen):
-        word += random.choice(\
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%*()_')
-    return word
