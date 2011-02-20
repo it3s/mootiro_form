74af6ee58887a5b9c05560734a79d3974f53de18
Edgar Z. Alvarenga <edgar@ymonad.com>
2011-02-19 22:07:26 -0200

    Get the entry id dynamically on form answers page

--- src/mootiro_form/static/js/form_entry.js
+++ src/mootiro_form/static/js/form_entry.js
@@ -1,5 +1,15 @@
 var field_template = "<span id='fieldLabel'>${label}:</span>${data}<br/>";
 
+function get_entry_data(id) {
+    return function () {
+        entry_data_url = 'http://' + url_root + route_url('entry', {action: 'data', id: id});
+        $.ajax({
+            url: entry_data_url, 
+            success: show_entry_data
+        }); 
+    }
+}
+
 function show_entry_data(entry) {
     $('#entryBox').dialog();
     $('#entryBox').html($.tmpl(field_template, entry));
