d2c31f589ef80ff18839e49bc6f9e66f3cbb6108
Nando Florestan <nandoflorestan@gmail.com>
2011-02-23 19:52:49 -0300

    Refactored form editing javascript. Sweet :)

--- src/mootiro_form/static/fieldtypes/TextField/editing.js
+++ src/mootiro_form/static/fieldtypes/TextField/editing.js
@@ -1,36 +1,39 @@
-TextField = new Object;
-TextField.template = $.template(
+// Constructor
+function TextField(props) {
+    if (props) {
+        this.props = props;
+    } else {
+        this.props = { // default values:
+            label : 'What is your favourite colour?',
+            defaul : 'Blue. No, wait -- green!'
+        };
+    }
+    if (!this.props.id) this.props.id = fieldId.next();
+//    id = props.id;
+//    this.label = props.label;
+//    this.defaul = props.defaul;
+}
+
+
+// Fields
+
+TextField.prototype.template = $.template(
     "<li id='${id}_container'><label for='${id}'>${label}</label>\n" +
     "<input type='text' name='${id}' id='${id}' value='${defaul}' />\n" +
     "</li>");
-TextField.optionsTemplate = $.template('');
-TextField.render = function(context) {
-  return $.tmpl(this.template, context);
+TextField.prototype.optionsTemplate = $.template('');
+
+
+// Methods
+
+TextField.prototype.render = function() {
+  return $.tmpl(this.template, this.props);
 };
-TextField.insert = function(properties) {
-  if (properties) {
-      this.properties = properties;
-  } else {
-      this.properties = TextField.properties;
-  }
-  this.properties.id = nextFieldId();
-  domNode = this.render(this.properties);
+TextField.prototype.insert = function(position) {
+  // for now, only insert at the end
+  domNode = this.render();
   $.event.trigger('AddField', [this, domNode]);
 };
-TextField.properties = { // default values:
-    label : 'What is your favourite colour?',
-    defaul : 'Blue. No, wait -- green!'
-};
 
-fieldtypes['TextField'] = TextField;
-
-/*
-lista = [];
-f = $('#FormFields')[0]
-f.edgar = {txt : 'walrus'};
-lista.push(f.edgar);
-console.log(lista[0].txt);
-lista[0].txt = 'aivuk';
-console.log(f.edgar.txt);
-console.log(lista[0].txt);
-*/
+// Register this type
+fieldTypes['TextField'] = TextField;
