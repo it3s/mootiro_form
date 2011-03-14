7473a6ba38d9b6e2843f0a46469b00508790c561
André Casimiro <ah.casimiro@gmail.com>
2011-03-14 18:55:33 -0300

    Changes public link visualization
    Feature #350

--- src/mootiro_form/static/js/form_edit.js
+++ src/mootiro_form/static/js/form_edit.js
@@ -372,8 +372,8 @@ FieldsManager.prototype.persist = function () {
             // Assume any deleted fields have been deleted at the DB
             instance.toDelete = [];
             // Shows the generated public link
-            $('#form_public_url').text(data.form_public_url)
-            $('#form_public_url').attr('href', data.form_public_url)
+            if (data.form_public_url)
+                $('#form_public_url').attr('value', data.form_public_url)
         }
     })
     .error(function (data) {
