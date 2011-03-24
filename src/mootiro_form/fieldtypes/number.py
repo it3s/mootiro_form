ee90d8d276ce2ba926c08cf580ba6e2ca7efdbba
André Casimiro <ah.casimiro@gmail.com>
2011-03-24 12:02:32 -0300

    Adds validation on saving new entries

--- src/mootiro_form/fieldtypes/number.py
+++ src/mootiro_form/fieldtypes/number.py
@@ -1,14 +1,12 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals  # unicode by default
+from __future__ import unicode_literals
 
 import colander as c
 import deform as d
-
 from mootiro_form import _
 from mootiro_form.fieldtypes import FieldType
 from mootiro_form.models import sas
 from mootiro_form.models.number_data import NumberData
-from mootiro_form.models.field_option import FieldOption
+import math
 
 
 class NumberField(FieldType):
@@ -26,7 +24,15 @@ class NumberField(FieldType):
                 .filter(NumberData.field_id == self.field.id) \
                 .filter(NumberData.entry_id == entry.id).first()
 
-        return data.value if data else ''
+        value = data.value
+
+        prec = int(self.field.get_option('precision'))
+        sep = self.field.get_option('separator')
+        if prec != 0:
+            if sep == ',':
+                value = str(value).replace('.', ',')
+
+        return value if data else ''
 
     def get_schema_node(self):
         params = dict()
@@ -43,14 +49,18 @@ class NumberField(FieldType):
 
         precision = int(self.field.get_option('precision'))
         if precision == 0:
-            type = c.Integer()
+            params['validator'] = get_validator('integer')
         else:
-            type = c.Decimal()
+            params['validator'] = get_validator('decimal',
+                separator=self.field.get_option('separator'),
+                precision=precision)
 
+        type = c.Str()
         sn = c.SchemaNode(type, **params)
         return sn
 
     def save_data(self, entry, value):
+        value = value.replace(',', '.')
         self.data = NumberData()
         self.data.field_id = self.field.id
         self.data.entry_id = entry.id
@@ -88,3 +98,49 @@ class NumberField(FieldType):
         # Add to the dict all the options of this field
         d.update({o.option: o.value for o in self.field.options})
         return d
+
+# Validators
+def get_validator(type, **kw):
+
+    if type == 'integer':
+        def integer_validator(node, value):
+            v = str(value)
+
+            try:
+                x = float(v.replace(',', '.'))
+            except ValueError:
+                raise c.Invalid(node, _('Not a number.'))
+            # if needed, at this point x is the float value to be saved.
+
+            if v.find('.') != -1 or v.find(',') != -1:
+                raise c.Invalid(node, _('Not an integer number.'))
+        
+        validator = integer_validator
+    
+    elif type == 'decimal':
+        def decimal_validator(node, value):
+            v = str(value)
+            sep = kw['separator']
+            prec = kw['precision']
+            
+            try:
+                x = float(v.replace(',', '.'))
+            except ValueError:
+                raise c.Invalid(node, _('Not a number.'))
+            # if needed, at this point x is the float value to be saved.
+
+            if (sep == '.' and v.find(',') != -1) or \
+               (sep == ',' and v.find('.') != -1):
+                raise c.Invalid(node, _('Wrong number format.'))
+
+            try:
+                dec = v.split(sep)[1] # capture decimal part
+            except IndexError:
+                return None # validation succeeded
+
+            if len(dec) > prec:
+                raise c.Invalid(node, _("Maximum of %(p)d decimals.") % {'p': prec})
+
+        validator = decimal_validator
+
+    return validator
