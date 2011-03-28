ab2d6c184ec6025acefebb9d1511fc2f246f4007
Jan Hackforth <janhack8@gmail.com>
2011-03-28 18:33:50 -0300

    adds parts of 'access period of a form'
    - calendar needs to be configured still
    - validation is missing

--- src/mootiro_form/models/form.py
+++ src/mootiro_form/models/form.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals  # unicode by default
 
-from sqlalchemy import Column, UnicodeText, Boolean, Integer, ForeignKey
+from datetime import datetime
+from sqlalchemy import Column, UnicodeText, Boolean, Integer, ForeignKey,\
+                       DateTime
 from sqlalchemy.orm import relationship, backref
 
 from mootiro_form.models import Base, id_column, now_column
@@ -16,6 +18,10 @@ class Form(Base):
     id = id_column(__tablename__)
     created = now_column()  # when was this record created
     modified = now_column()  # when was this form saved
+    # from then on the form will be accessible 
+    start_date = Column(DateTime, nullable=True)
+    # until then the form will be accessible
+    end_date = Column(DateTime, nullable=True)
     name = Column(UnicodeText(255), nullable=False)
     submit_label = Column(UnicodeText(255), nullable=True)
     description = Column(UnicodeText)
@@ -43,6 +49,22 @@ class Form(Base):
         num_entries = sas.query(Entry).filter(Entry.form_id == self.id).count()
         return num_entries
 
+    STATUS_EDITING = 'editing' # form is not public yet
+    STATUS_BEFORE = 'before'  # form is public but before start date
+    STATUS_DURING = 'during'  # form is public and entries may be created
+    STATUS_AFTER = 'after'  # form is public but after end date
+
+    @property
+    def status(self):
+        '''Returns a form status code.'''
+        if not self.public:
+            return self.STATUS_EDITING
+        if self.start_date and datetime.utcnow() < self.start_date:
+            return self.STATUS_BEFORE
+        if self.end_date and datetime.utcnow() > self.end_date:
+            return self.STATUS_AFTER
+        return self.STATUS_DURING
+
     def to_json(self):
         return {'form_id': self.id,
                 'form_name': self.name or 'Untitled form',
@@ -50,6 +72,7 @@ class Form(Base):
                 'form_description': self.description,
                 'form_slug': self.slug,
                 'form_public': self.public,
+                'form_status': self.status,
                 'form_thanks_message': self.thanks_message,
                 'form_created': unicode(self.created)[:16],
                 'form_modified': unicode(self.modified)[:16],
