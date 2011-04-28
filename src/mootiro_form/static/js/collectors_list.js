2e4323efa3fcbdf45277ee46d03627ca86a41760
André Casimiro <ah.casimiro@gmail.com>
2011-04-28 12:27:26 -0300

    Added basic jQuery templates for collectors page
    NEXT: implement float window and tabs for public link collector in form_collectors.genshi

---
+++ src/mootiro_form/static/js/collectors_list.js
@@ -0,0 +1,6 @@
+$('#middle').append($('#collectors_template').tmpl());
+$('#collectors_rows').append($('#collectors_rows_template').tmpl({}));
+
+var listTable = $('#CollectorsListTable');
+listTable.find('tr td:nth-child(2n)').addClass('darker');
+listTable.find('thead th:nth-child(2n)').addClass('darker');
