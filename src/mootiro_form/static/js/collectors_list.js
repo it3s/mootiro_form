ebdb60604d42b4cf5cb9023151227af4516b6c5c
André Casimiro <ah.casimiro@gmail.com>
2011-05-04 12:03:28 -0300

    Adds first jquery float window with its tabs

--- src/mootiro_form/static/js/collectors_list.js
+++ src/mootiro_form/static/js/collectors_list.js
@@ -4,3 +4,8 @@ $('#collectors_rows').append($('#collectors_rows_template').tmpl({}));
 var listTable = $('#CollectorsListTable');
 listTable.find('tr td:nth-child(2n)').addClass('darker');
 listTable.find('thead th:nth-child(2n)').addClass('darker');
+
+
+// Float Windows and Tabs
+$('#PublicLinkWindow').dialog();
+$('#PublicLinkTabs').tabs();
