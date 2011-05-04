2e1b2d86579d8d8e1ddf3e01734552d73eb12923
Nando Florestan <nandoflorestan@gmail.com>
2011-05-04 01:35:30 -0300

    Extraction working minimally.

--- src/translate_js.sh
+++ src/translate_js.sh
@@ -1,19 +1,24 @@
 #! /bin/sh
 # http://babel.edgewall.org/wiki/Documentation/0.9/cmdline.html
 
+DOMAIN=js_mf
 OUTDIR=mootiro_form/locale
-POTFILE=$OUTDIR/js_mf.pot
+POTFILE=$OUTDIR/$DOMAIN.pot
+
+# This creates the catalogues, based on the POTFILE -- but this should run only once:
+# pybabel init -D $DOMAIN -i $POTFILE -d $OUTDIR -l en
+# pybabel init -D $DOMAIN -i $POTFILE -d $OUTDIR -l pt_BR
 
 # Regenerate the .pot file
-pybabel extract -F $OUTDIR/js_mapping.conf -k "tr" --omit-header -w 100 --sort-by-file -o $POTFILE mootiro_form/static
-
+pybabel extract -F $OUTDIR/js_mapping.conf -k "tr" --omit-header -w 100 \
+    --sort-by-file -o $POTFILE mootiro_form/static && \
 # Update the .po files
-#pybabel update -D js_mf -i $POTFILE -d $OUTDIR -N
+pybabel update -D $DOMAIN -i $POTFILE -d $OUTDIR -N -l en && \
+pybabel update -D $DOMAIN -i $POTFILE -d $OUTDIR -N -l pt_BR
 
 # Edit the pt_BR file
 # sudo apt-get install poedit
 #echo Running poedit...
-#poedit $OUTDIR/en/LC_MESSAGES/js_mf.po
+#poedit $OUTDIR/en/LC_MESSAGES/$DOMAIN.po
 
-# Compile the .mo
-# ./setup.py compile_catalog # makes translations available to the app
+# TODO: Compile to JSON
