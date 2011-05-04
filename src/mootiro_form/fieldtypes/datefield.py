579dd93cdf64bdca29925e886a52d374e09579a8
Edgar Z. Alvarenga <edgar@ymonad.com>
2011-05-04 10:46:41 -0300

    Changing the date picker widget

--- src/mootiro_form/fieldtypes/datefield.py
+++ src/mootiro_form/fieldtypes/datefield.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals  # unicode by default
 import colander as c
 import deform as d
 from datetime import datetime
+import json
 
 from mootiro_form import _
 from mootiro_form.fieldtypes import FieldType
@@ -51,7 +52,7 @@ df.add('1999-09-30', '%Y-%m-%d', 'yy-mm-d')
 df.add('30.9.99', '%d.%m.%y', 'dd.m.y', lambda date: '{0}.{1}.{2}'. \
                 format(date.day, str(date.month), date.year))
 # 30.09.99
-df.add('30.09.99', '%d.%m.%y', 'dd.m.y')
+df.add('30.09.99', '%d.%m.%y', 'dd.mm.y')
 # 30.9.1999
 df.add('30.9.1999', '%d.%m.%Y', 'dd.m.yy', lambda date: '{0}.{1}.{2}'. \
                 format(date.day, str(date.month), date.year))
@@ -61,16 +62,16 @@ df.add('30.09.1999', '%d.%m.%Y', 'dd.mm.yy')
 df.add('Sep 1, 99', "%b %d, %y" ,'M d, y', lambda date: \
         date.strftime('%b {0}, %y').format(str(date.day)))
 # Sep 01, 99
-df.add('Sep 01, 99', "%b %d, %y" ,'M d, y')
+df.add('Sep 01, 99', "%b %d, %y" ,'M dd, y')
 # Sep 1, 1999
 df.add('Sep 1, 1999', "%b %d, %Y" ,'M d, yy', lambda date: \
         date.strftime('%b {0}, %y').format(str(date.day)))
 # Sep 01, 1999
-df.add('Sep 01, 1999', "%b %d, %Y" ,'M d, yy')
+df.add('Sep 01, 1999', "%b %d, %Y" ,'M dd, yy')
 # 01. Sep. 99
-df.add('01. Sep. 99', "%d. %b. %y" ,'d. M. y')
+df.add('01. Sep. 99', "%d. %b. %y" ,'dd. M. y')
 # 01. Sep. 1999
-df.add('01. Sep. 1999', "%d. %b. %Y" ,'d. M. yy')
+df.add('01. Sep. 1999', "%d. %b. %Y" ,'dd. M. yy')
 # September 30, 1999
 df.add('September 30, 1999', "%B %d, %Y" ,'MM d, yy')
 # 30. September 1999
@@ -84,6 +85,9 @@ class DateField(FieldType):
     defaultValue = dict(defaul='',
                         input_date_format='%Y-%m-%d',
                         export_date_format='%Y-%m-%d',
+                        month_selector=False,
+                        year_selector=False,
+                        show_week=False,
                         required=False)
 
     def initJson(self):
@@ -107,10 +111,30 @@ class DateField(FieldType):
     def get_schema_node(self):
         widget = d.widget.DateInputWidget(template='form_date')
         date_default = self.field.get_option('defaul')
+        month_selector = True if self.field.get_option('month_selector') == 'true' else False
+        year_selector = True if self.field.get_option('year_selector') == 'true' else False
+        show_week = True if self.field.get_option('show_week') == 'true' else False
+
+        calendar_config= dict(changeMonth=month_selector,
+                              changeYear=year_selector,
+                              showWeek=show_week,
+                              dateFormat=df.formats[int(self.field.get_option \
+                                    ('input_date_format'))]['js'])
+
+        calendar_config_json = json.dumps(calendar_config)
+
         if date_default != '':
             default = {'default':date_default}
         else:
             default = {}
+
+        def date_validation(node, val):
+            try:
+                date = datetime.strptime(val,
+                    df.formats[int(self.field.get_option \
+                        ('input_date_format'))]['py'])
+            except ValueError:
+                raise c.Invalid(node, _("Invalid date format"))
 
         if self.field.required:
             sn = c.SchemaNode(c.Str(), title=self.field.label,
@@ -120,6 +144,8 @@ class DateField(FieldType):
                 date_format_py=df.formats[int(self.field.get_option \
                     ('input_date_format'))]['py'],
                 description=self.field.description, widget=widget,
+                calendar_config=calendar_config_json,
+                validator=date_validation,
                 **default)
         else:
             sn = c.SchemaNode(c.Str(), title=self.field.label,
@@ -130,6 +156,8 @@ class DateField(FieldType):
                 date_format_py=df.formats[int(self.field.get_option \
                     ('input_date_format'))]['py'],
                 description=self.field.description, widget=widget,
+                calendar_config=calendar_config_json,
+                validator=date_validation,
                 **default)
         return sn
 
@@ -158,6 +186,9 @@ class DateField(FieldType):
         self.field.required = options['required']
         self.field.description = options['description']
         self.field.position = options['position']
+        self.save_option('month_selector', options['month_selector'])
+        self.save_option('year_selector', options['year_selector'])
+        self.save_option('show_week', options['show_week'])
         self.save_option('input_date_format', options['input_date_format'])
         self.save_option('export_date_format', options['export_date_format'])
         # "default" is a reserved word in javascript. Gotta change that name:
@@ -174,9 +205,10 @@ class DateField(FieldType):
             required=self.field.required,
             input_date_format=self.field.get_option('input_date_format'),
             export_date_format=self.field.get_option('export_date_format'),
+            month_selector=True if self.field.get_option('month_selector') == 'true' else False,
+            year_selector=True if self.field.get_option('year_selector') == 'true' else False,
+            show_week=True if self.field.get_option('show_week') == 'true' else False,
             description=self.field.description,
             defaul=self.field.get_option('defaul'),
         )
-        # Add to the dict all the options of this field
-        d.update({o.option: o.value for o in self.field.options})
         return d
