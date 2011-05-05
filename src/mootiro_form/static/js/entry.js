2a09618cc5ce43f86bd61b975e305c4e9bfd3b97
Edgar Z. Alvarenga <edgar@ymonad.com>
2011-05-05 08:59:38 -0300

    Disable enter on firefox

--- src/mootiro_form/static/js/entry.js
+++ src/mootiro_form/static/js/entry.js
@@ -1,6 +1,15 @@
-function noenter() {
-    return !(window.event && window.event.keyCode == 13);
-}
 $(function () {
-    $('input').keypress(noenter);
+    $('body').keypress(function (e) {
+        return disableEnterKey(e);   
+    });
 });
+
+function disableEnterKey(e) {
+     var key;      
+     if(window.event)
+          key = window.event.keyCode; //IE
+     else
+          key = e.which; //firefox      
+
+     return (key != 13);
+}
