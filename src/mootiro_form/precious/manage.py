d858995de57085fc701072b45c767f568d37a3a9
Nando Florestan <nandoflorestan@gmail.com>
2011-05-09 21:04:22 -0300

    Corrections to the form table in migration.

--- src/mootiro_form/precious/manage.py
+++ src/mootiro_form/precious/manage.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
-from migrate.versioning.shell import main
-main(debug='False')
+
+# Lines below have been commented out because of a conflict with paster.
+
+#from migrate.versioning.shell import main as migrate_main
+#migrate_main(debug='False')
