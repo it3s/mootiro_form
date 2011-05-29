8c16a9af04323635e7ec7ca84470b568c1eddb39
Nando Florestan <nandoflorestan@gmail.com>
2011-05-29 12:34:09 -0300

    Add a script to use the CherryPy WSGI server
    ...because the Paste HTTP server has been printing weird
    error messages for the more complex pages.

---
+++ src/cherryserver.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''Serves Mootiro Form using the high-performance CherryPy WSGI server.
+(I don't think the Paste http server is very good,
+it has been printing mysterious error messages on the console.)
+
+Before running this script, install CherryPy 3.2 or later:
+
+    easy_install -UZ cherrypy
+
+'''
+
+from __future__ import unicode_literals  # unicode by default
+from cherrypy.wsgiserver import CherryPyWSGIServer
+from pyramid.paster import get_app
+
+app = get_app('development.ini', 'mootiro_form')
+port = 6545
+server = CherryPyWSGIServer(('0.0.0.0', port), app, numthreads=10, max=10,
+    request_queue_size=200, timeout=120,
+)
+
+if __name__ == '__main__':
+    print('Running Mootiro Form under CherryPy on http://localhost:{}/' \
+        .format(port))
+    try:
+        server.start()
+    except (KeyboardInterrupt, SystemExit) as e:
+        server.stop()
+        print('\nStopping CherryPy server.')
