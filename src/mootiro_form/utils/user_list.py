20dcf43fd7ab445992ceb4c4f4628277029b29b6
Nando Florestan <nandoflorestan@gmail.com>
2011-11-07 15:24:44 -0200

    Include password hash in users CSV.

--- src/mootiro_form/utils/user_list.py
+++ src/mootiro_form/utils/user_list.py
@@ -9,16 +9,15 @@ def users_to_csv(filename):
 
     $ ./shell.sh
     from mootiro_form.utils.user_list import users_to_csv
-    users_to_csv('test.csv')
-    users_to_csv('/tmp/form_users.csv')
+    users_to_csv('/tmp/form-users.csv')
     # Locally:
     scp mootiro_form@form.mootiro.org:/tmp/form_users.csv .
     '''
     from bag.csv import CsvWriter
     print('Writing users to ' + filename)
     w = CsvWriter(file=open(filename, 'w'))
-    w.put(['nickname', 'name', 'email'])
+    w.put(['nickname', 'name', 'email', 'password'])
     for user in sas.query(User):
-        w.put([user.nickname, user.real_name, user.email])
+        w.put([user.nickname, user.real_name, user.email, user.password_hash])
     w.close()
     print('Done.')
