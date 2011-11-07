445b58314de531752c4313d04903d77549ba78d7
Nando Florestan <nandoflorestan@gmail.com>
2011-11-07 16:35:58 -0200

    Export more user columns.

--- src/mootiro_form/utils/user_list.py
+++ src/mootiro_form/utils/user_list.py
@@ -11,13 +11,16 @@ def users_to_csv(filename):
     from mootiro_form.utils.user_list import users_to_csv
     users_to_csv('/tmp/form-users.csv')
     # Locally:
-    scp mootiro_form@form.mootiro.org:/tmp/form_users.csv .
+    scp mootiro_form@form.mootiro.org:/tmp/form-users.csv .
     '''
     from bag.csv import CsvWriter
     print('Writing users to ' + filename)
     w = CsvWriter(file=open(filename, 'w'))
-    w.put(['nickname', 'name', 'email', 'password'])
+    w.put(['nickname', 'name', 'email', 'is_email_validated', 'password',
+        'created', 'default_locale'])
     for user in sas.query(User):
-        w.put([user.nickname, user.real_name, user.email, user.password_hash])
+        w.put([user.nickname, user.real_name, user.email,
+            '1' if user.is_email_validated else '0',
+            user.password_hash, user.created, user.default_locale])
     w.close()
     print('Done.')
